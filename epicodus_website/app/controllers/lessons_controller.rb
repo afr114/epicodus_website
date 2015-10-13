e68a0682a70825b902dfef6bd8f9a1e6485a24d1
abdul rahimi & clinton herrick <student@epicodus.com>
2015-10-13 12:01:36 -0700

    add update route

--- epicodus_website/app/controllers/lessons_controller.rb
+++ epicodus_website/app/controllers/lessons_controller.rb
@@ -21,6 +21,19 @@ class LessonsController < ApplicationController
     @lesson = Lesson.find(params[:id])
   end
 
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      redirect_to lessons_path
+    else
+      render :edit
+    end
+  end
+
   private
   def lesson_params
     params.require(:lesson).permit(:name, :content)
