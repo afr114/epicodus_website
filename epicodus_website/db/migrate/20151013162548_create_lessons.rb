3da17494998001e8bfcef6180ade31dc61088342
abdul rahimi & clinton herrick <student@epicodus.com>
2015-10-13 11:09:12 -0700

    add show route

---
+++ epicodus_website/db/migrate/20151013162548_create_lessons.rb
@@ -0,0 +1,11 @@
+class CreateLessons < ActiveRecord::Migration
+  def change
+    create_table :lessons do |t|
+
+      t.column :name, :string
+      t.column :content, :text
+
+      t.timestamps
+    end
+  end
+end
